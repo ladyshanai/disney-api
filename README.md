66594f1837698c3188205f7a11a70608b060d642
Paula <paulalezcano21@gmail.com>
2025-04-08 11:35:40 -0300

    feat(disneyapi): connection to supabase

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # disney-api
 API to explore the world of Disney, allowing users to discover and modify characters, and understand which movies they appeared in.
+
+* Java version 17
+* Springboot version 3.4.3
+
---
+++ src/test/java/com/disney/disneyapi/DisneyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.disneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
